978713b132b065ecef02fc052552e5e0fab5c343
Mrabet Marouane <144802049+MrabetMarouane@users.noreply.github.com>
2024-03-11 15:54:05 +0100

    Update Scanner.js

--- Scanner.js
+++ Scanner.js
@@ -1,13 +1,13 @@
-import { View, Text, StyleSheet, Button, Linking } from 'react-native';
 import React, { useEffect, useState } from 'react';
+import { View, Text, StyleSheet, Button } from 'react-native';
 import { Camera } from 'expo-camera';
 import { useNavigation } from '@react-navigation/native';
-
+import axios from 'axios'; // Importer la bibliothèque axios
 
 export default function Scanner() {
     const [hasPermission, setHasPermission] = useState(null);
     const [scanned, setScanned] = useState(false);
-    const navigation= useNavigation();
+    const navigation = useNavigation();
 
     useEffect(() => {
         (async () => {
@@ -16,31 +16,62 @@ export default function Scanner() {
         })();
     }, []);
 
-   
-
-    const handleBarCodeSanned = ({ type, data }) => {
+    const handleBarCodeScanned = ({ type, data }) => {
         setScanned(true);
-        //Linking.openURL(data);
-        alert(`Bar code with Type ${type} and data ${Linking.openURL(`${data}`)} has been scanned!`);
+        if (data.startsWith('https://')) {
+            // Si le QR Code contient un lien HTTPS, ouvrez le lien
+            Linking.openURL(data);
+        } else {
+            // Sinon, affichez les données du QR Code dans une alerte
+            alert(`Bar code with Type ${type} and data :\n ${data}\n has been scanned!`);
+        }
     };
 
-     // Si la permission de la caméra n'est pas encore accordée, affichez un message de chargement
-     if (hasPermission === null) {
+    // Fonction pour activer la prise
+    const activaterPrise = async () => {
+        try {
+            const response = await axios.get('http://192.168.5.67/rpc/Switch.Set?id=0&on=true');
+            console.log(response.data); // Affiche la réponse de la requête HTTP
+            alert('Prise activée !');
+        } catch (error) {
+            console.error('Erreur lors de l\'activation de la prise:', error);
+            alert('Erreur lors de l\'activation de la prise');
+        }
+    };
+
+    // Fonction pour désactiver la prise
+    const desactivaterPrise = async () => {
+        try {
+            const response = await axios.get('http://192.168.5.67/rpc/Switch.Set?id=0&on=false');
+            console.log(response.data); // Affiche la réponse de la requête HTTP
+            alert('Prise désactivée !');
+        } catch (error) {
+            console.error('Erreur lors de la désactivation de la prise:', error);
+            alert('Erreur lors de la désactivation de la prise');
+        }
+    };
+
+    if (hasPermission === null) {
         return <Text>Demande de permission d'accès à la caméra...</Text>;
     }
-    // Si la permission est refusée, affichez un message indiquant que l'accès à la caméra est nécessaire
+
     if (hasPermission === false) {
         return <Text>L'accès à la caméra est nécessaire pour utiliser cette fonctionnalité.</Text>;
     }
 
-    // Affichez le scanner de codes-barres
     return (
         <View style={styles.container}>
             <Camera
-                onBarCodeScanned={scanned ? undefined : handleBarCodeSanned}
+                onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
                 style={StyleSheet.absoluteFillObject}
             />
-            {scanned && (<Button title='Tap to Scan Again' onPress={() => setScanned(false)} />)}
+            {scanned && (
+                <View>
+                    <Button title='Tap to Scan Again' onPress={() => setScanned(false)} />
+                    <Button title='Activer la prise' onPress={activaterPrise} />
+                    <Button title='Désactiver la prise' onPress={desactivaterPrise} />
+                </View>
+            )}
         </View>
     );
 }
